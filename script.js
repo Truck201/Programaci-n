5a5540ddde4548ece02418c4cb52be7f671878ad
Joaquín A Gandini <joaquingandini22@gmail.com>
2024-04-12 01:34:13 -0300

    Fix code, adjust

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let slides = document.querySelectorAll(".itemCarrousel");
-    let currentSlide = 0;
-    
-    function showSlide(index) {
-      slides.forEach((slide) => {
-        slide.classList.remove("active");
-      });
-      slides[index].classList.add("active");
-    }
-    
-    function nextSlide() {
-      currentSlide = (currentSlide + 1) % slides.length;
-      showSlide(currentSlide);
-    }
-    
+document.addEventListener("DOMContentLoaded", function () {
+  let slides = document.querySelectorAll(".itemCarrousel");
+  let currentSlide = 0;
+
+  function showSlide(index) {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+    slides[index].classList.add("active");
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
     showSlide(currentSlide);
-    setInterval(nextSlide, 4500); // Cambia de slide cada 5 segundos
-  });
-  
+  }
+
+  showSlide(currentSlide);
+  setInterval(nextSlide, 4500); // Cambia de slide cada 5 segundos
+});
